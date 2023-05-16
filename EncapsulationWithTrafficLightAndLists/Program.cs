269a3289a5e48ec298edc5011a0b411fb519948c
GET Academy <terje@getacademy.no>
2023-05-16 08:51:36 +0200

    Initial commit

---
+++ EncapsulationWithTrafficLightAndLists/Program.cs
@@ -0,0 +1,6 @@
+﻿using EncapsulationWithTrafficLightAndLists.TrafficLightStuff;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+TrafficLightConsole.Write(true, false, true);
---
+++ EncapsulationWithTrafficLightAndLists/TrafficLightStuff/TrafficLightConsole.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace EncapsulationWithTrafficLightAndLists.TrafficLightStuff
+{
+    internal class TrafficLightConsole
+    {
+        public static void Write(bool isRedOn, bool isYellowOn, bool isGreenOn)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("┏━┓");
+            DrawLine(isRedOn ? ConsoleColor.Red : ConsoleColor.Gray);
+            DrawLine(isYellowOn ? ConsoleColor.Yellow : ConsoleColor.Gray);
+            DrawLine(isGreenOn ? ConsoleColor.Green : ConsoleColor.Gray);
+            Console.WriteLine("┗━┛");
+        }
+
+        private static void DrawLine(ConsoleColor color)
+        {
+            var foregroundColor = Console.ForegroundColor;
+            Console.Write("┃");
+            Console.ForegroundColor = color;
+            Console.Write("●");
+            Console.ForegroundColor = foregroundColor;
+            Console.WriteLine("┃");
+        }
+    }
+}
